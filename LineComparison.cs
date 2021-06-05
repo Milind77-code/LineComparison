3e205daf9ec89541b9b0e3be5de06b5d83ef3108
Milind_77 <milinddhote77@gmail.com>
2021-06-05 19:36:01 +0530

    [milind] Add: Using OOP concepts

--- LineComparison.cs
+++ LineComparison.cs
@@ -2,51 +2,49 @@
 
 namespace LineComparison
 {
-    public class LineComparison
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("<------Welcome to Line Comparison Program------>");
-            Console.WriteLine("------------------------------------------------");
-            //Variables
-            int x1, y1, x2, y2, x3, y3, x4, y4;
+	public class LineComparison
+	{
+		//Variables Declaration
+		public int x1, y1, x2, y2;
+		public String line;
+		public static double length1, length2;
 
-            // User inputs the coordinates
-            Console.WriteLine("Enter 1st co-ordinate of x-axis of First Line : ");
-            x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 1st co-ordinate of y-axis of First Line : ");
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 2nd co-ordinate of x-axis of First Line : ");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 2nd co-ordinate of y-axis of First Line : ");
-            y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a 1st co-ordinate of x-axis of Second Line : ");
-            x3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a 1st co-ordinate of y-axis of Second Line : ");
-            y3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a 2nd co-ordinate of x-axis of Second Line : ");
-            x4 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a 2nd co-ordinate of y-axis of Second Line : ");
-            y4 = Convert.ToInt32(Console.ReadLine());
+		/// <summary>
+		/// Method for computing the length of lines
+		/// </summary>
+		/// <param name="line"></param>
+		/// <param name="x1"></param>
+		/// <param name="y1"></param>
+		/// <param name="x2"></param>
+		/// <param name="y2"></param>
+		/// <returns></returns>
+		public double lengthOfLine(String line, int x1, int y1, int x2, int y2)
+		{
 
-            //Print the coordinates
-            Console.WriteLine("End Points of First Line Segment are = " + "(" + x1 + "," + y1 + ")" + "(" + x2 + "," + y2 + ")");
-            Console.WriteLine("End Points of Second Line Segment are = " + "(" + x3 + "," + y3 + ")" + "(" + x4 + "," + y4 + ")");
-            
-            // Convert double into String
-            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            String str1 = Convert.ToString(length1);
-            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            String str2 = Convert.ToString(length2);
-            
-            // Prints length of 2 line segment
-            Console.WriteLine("Length of a First Line Segment = " + length1);
-            Console.WriteLine("Length of a Second Line Segment = " + length2);
-
-            // Using CompareTo method to compare 2 lengths
-            Console.WriteLine(str1.CompareTo(str2)); // Prints 0;(because str1=str2)
-            Console.WriteLine(str1.CompareTo(str2)); // Prints 1;(because str1>str2)
-            Console.WriteLine(str2.CompareTo(str1)); // Prints -1;(because str1<str2)
-        }
-    }
+			Console.WriteLine("------------------------------------------------");
+			double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+			Console.WriteLine("End Points of Line Segments are = " + "(" + x1 + "," + y1 + ")" + "(" + x2 + "," + y2 + ")");
+			Console.WriteLine("Length of " + line + " = " + length);
+			return length;
+		}
+		/// <summary>
+		/// Calling from main method
+		/// </summary>
+		/// <param name="args"></param>
+		public static void Main(String[] args)
+		{
+			Console.WriteLine("<------Welcome to Line Comparison Program------>");
+			LineComparison l1 = new LineComparison();
+			double length1 = l1.lengthOfLine("Line 1", 7, 5, 3, 1);
+			String str1 = Convert.ToString(length1);
+			LineComparison l2 = new LineComparison();
+			double length2 = l2.lengthOfLine("Line 2", 2, 4, 5, 0);
+			String str2 = Convert.ToString(length2);
+			Console.WriteLine("------------------------------------------------");
+			Console.WriteLine(str1.Equals(str2));//True or False
+			Console.WriteLine(str1.CompareTo(str2));//prints 1
+			Console.WriteLine(str2.CompareTo(str1));//prints -1
+			Console.WriteLine("------------------------------------------------");
+		}
+	}
 }
